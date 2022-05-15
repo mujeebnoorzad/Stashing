afc96ed226dda4dee42de4036212aad5687033bd
mujeeb <mujeeb.noorzad@gmail.com>
2022-05-15 08:03:27 -0700

    the bsic structure of the car is ready, working on advance features

---
+++ src/calss03/BasicFeature.java
@@ -0,0 +1,9 @@
+package calss03;
+
+public class BasicFeature {
+    String engineCapac;
+    String Color;
+    String Doors;
+    String Headlights;
+    String regNo;
+}
---
+++ src/calss03/Main.java
@@ -0,0 +1,8 @@
+package calss03;
+
+public class Main {
+    public static void main(String[] args) {
+       BasicFeature car=new BasicFeature();
+
+    }
+}
