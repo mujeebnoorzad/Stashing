fdd478f14fdb54decc0aea5c2a3c47fdb4391322
mujeeb <mujeeb.noorzad@gmail.com>
2022-05-15 09:05:30 -0700

    The red car

--- src/calss03/Main.java
+++ src/calss03/Main.java
@@ -3,6 +3,11 @@ package calss03;
 public class Main {
     public static void main(String[] args) {
        BasicFeature car=new BasicFeature();
+        car.Color="red";
+        car.Doors="4";
+        car.engineCapac="V8";
+        car.Headlights="blue";
+        car.regNo="tuzik";
 
     }
 }
